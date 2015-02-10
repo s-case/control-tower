6c9b27c0187818375059a72e55c0f520f725941b
Themis Mavridis <themismavridis@gmail.com>
2015-02-10 14:16:20 +0200

    initial commit

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,13 @@ module.exports = function(app, passport) {
 		});
 	});
 
+	// PROFILE SECTION =========================
+	app.get('/profile_alert', isLoggedIn, function(req, res) {
+		res.render('profile_alert.ejs', {
+			user : req.user
+		});
+	});
+
 	// LOGOUT ==============================
 	app.get('/logout', function(req, res) {
 		req.logout();
@@ -79,8 +86,7 @@ module.exports = function(app, passport) {
                                 res.redirect('/profile');
                             });
 	});
-
-	// =============================================================================
+// =============================================================================
 // Delete account =============================================================
 // =============================================================================
 // used to unlink accounts. for social accounts, just remove the token
@@ -104,12 +110,17 @@ module.exports = function(app, passport) {
 
 		connection.query(CheckOwnQuery, function(err, rows) {
                          	if (err) throw err;
-                         	var newmessage = "You still own " ;
+                         	var newmessage;
 							for(var i in rows){
-								newmessage =newmessage+rows[i].project_name + " and ";
+								if(i==0){
+									newmessage =rows[i].project_name + " and ";
+								}
+								else {
+									newmessage =newmessage+rows[i].project_name + " and ";
+								}
 							}
-							newmessage = newmessage.substring(0,newmessage.length-4);
 							if(newmessage!=null){
+								newmessage = newmessage.substring(0,newmessage.length-4);
 								connection.query("SELECT * FROM " + dbconfig.users_table + " WHERE id = '" + user.id + "'", function(err, rows){
 					                    if (err)
 					                        return done(err);
@@ -121,9 +132,8 @@ module.exports = function(app, passport) {
 				                            var updateQuery = "UPDATE " + dbconfig.users_table + " SET " +
 				                                "`message` = '" + userProfile.Message + "' " +
 				                                "WHERE `id` = '" + user.id + "' LIMIT 1";
-				                            console.log(updateQuery);
 				                            connection.query(updateQuery, function(err, rows) {
-				                               console.log(rows);
+				                              
 				                            });
 					                    }
 									
@@ -135,15 +145,70 @@ module.exports = function(app, passport) {
 						                                "`github_token` = '" + undefined + "' " +                               
 						                                "WHERE `github_id` = " + user.github_id + " LIMIT 1";
 								connection.query(DeleteQuery, function(err, rows) {
-						                                res.redirect('/');
+						                                
 						                            });
 							}
-							res.redirect('/profile');
+							if(newmessage!=null){
+								res.redirect('/profile_alert');
+							}else{
+								res.redirect('/');
+							}
 
 
 						});
 	});
+	// =============================================================================
+	// Refresh S-CASE token ACCOUNTS =============================================================
+	// =============================================================================
+	// used to unlink accounts. for social accounts, just remove the token
+	// for local account, remove email and password
+	// user account will stay active in case they want to reconnect in the future
+		var crypto = require('crypto');
+		var base64url = require('base64url');
+
+		function scasetokenCreate(size){
+		    return base64url(crypto.randomBytes(size));
+
+		}
+		var mysql = require('mysql');
+		var dbconfig = require('../config/database');
+		//var connection = mysql.createConnection(dbconfig.connection);
+		var connection = require('../config/ConnectConstant.js');
+		connection.query('USE ' + dbconfig.database);
+
+		// github -------------------------------
+		app.get('/refresh/github', isLoggedIn, function(req, res) {
+			var user            = req.user;
+			connection.query("SELECT * FROM " + dbconfig.users_table + " WHERE id = '" + user.id + "'", function(err, rows){
+						
+	                    if (rows.length > 0) {
+	                        user = rows[0];
+	                        var scasetoken = scasetokenCreate(35)
+	                        user.scase_token=scasetoken;
+	                        var updateQuery = "UPDATE " + dbconfig.users_table + " SET " +
+	                                "`scase_token` = '" + user.scase_token + "' " +
+	                                "WHERE `id` = '" + user.id + "' LIMIT 1";
+	                        connection.query(updateQuery, function(err, rows) {
+	                          
+	                        });
+	                        
+	                    }
+	                    res.redirect('/profile');
+	        });
+
+
+		});
+
+
+
+
+
+
+
+
 };
+
+
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
