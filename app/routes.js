e9f0cc5713a0d6ac2d1de020a4cb24495159e159
Themis Mavridis <themismavridis@gmail.com>
2015-02-25 14:47:49 +0200

    add owner and collaborator

--- app/routes.js
+++ app/routes.js
@@ -408,14 +408,15 @@ module.exports = function(app, passport) {
                     			}
 	                    	}
                     }
-                    callback();
+                    callback(collabflag);
         		});
 			}
-			checkIfCollab(function(){
+			checkIfCollab(function(collabflag){
 				if(collabflag==true){
 					var owners;//variable to display owners
 					var collaborators;//variable to display collaborators
-					function displayOwnersCollaborators (callback){
+					
+					function displayOwners (callback){
 						var selectOwnersQuery = "SELECT `github_name` FROM " + dbconfig.users_table +" JOIN " + dbconfig.owners_table + 
 						" ON "+ dbconfig.users_table + ".id=" +  dbconfig.owners_table+".user_id "+ "JOIN "+ dbconfig.projects_table+
 						" ON "+ dbconfig.projects_table + ".project_id=" + dbconfig.owners_table + ".project_id" + 
@@ -426,8 +427,11 @@ module.exports = function(app, passport) {
 			                    	console.log(rows);
 			                    	owners=rows;
 			                    }
-			                }
-		        		);
+			                    callback(owners);
+			            });
+					}
+					
+					function displayCollaborators (callback){
 		        		var selectCollaboratorsQuery = "SELECT `github_name`," + dbconfig.collaborators_table + ".id AS `collab_id` FROM " + dbconfig.users_table +" JOIN " + dbconfig.collaborators_table + 
 						" ON "+ dbconfig.users_table + ".id=" +  dbconfig.collaborators_table+".user_id "+ "JOIN "+ dbconfig.projects_table+
 						" ON "+ dbconfig.projects_table + ".project_id=" + dbconfig.collaborators_table + ".project_id" + 
@@ -439,26 +443,28 @@ module.exports = function(app, passport) {
 		                    	console.log(rows);
 		                    	collaborators=rows;
 		                    }
-		                    callback();
+		                    callback(collaborators);
 		        		});
 					}
-					displayOwnersCollaborators(function(){
-						console.log("owners"+owners);
-						console.log("collabs"+collaborators);
-						res.render('projectSpecific.ejs', {
-									ownersnames : owners,
-									collaboratorsnames : collaborators,
-									projectname : proj_name,
-									username : user.github_name
+					
+					displayOwners(function(){
+						//console.log("owners"+owners);
+						//console.log("collabs"+collaborators);
+						displayCollaborators(function(){
+							res.render('projectSpecific.ejs', {
+									ownersnames : owners,//we return owners
+									collaboratorsnames : collaborators,//we return collaborators
+									projectname : proj_name,//we return project name
+									username : user.github_name//we return the active's User name (it is used in order be able to remove your own account from the project)
 								});
-
+						});
 					});
 				}
 			});	
 		}
 	});
 	// =============================================================================
-	// Remove a Collaborator from a Project I own ==================================
+	// Remove a Collaborator from a Project I own or If I want to remove myself ==================================
 	// =============================================================================
 	app.get('/removeCollab/github', isLoggedIn, function(req, res) {
 		var user = req.user;
@@ -474,7 +480,29 @@ module.exports = function(app, passport) {
 	                    res.redirect('/manageprojects/github'+'?project_name='+proj_name);
                 });
 			}
-		});	
+			else{
+				var getCollabInfo = "SELECT user_id FROM " + dbconfig.collaborators_table +
+					" WHERE " + dbconfig.collaborators_table + ".id=" + "'" + collab_id + "'";
+				console.log(getCollabInfo);
+				handleDisconnect();
+				connection.query(getCollabInfo, function(err, rows){
+						if(rows.length>0){
+							if(rows[0].user_id==user.id){
+								 var removeCollabQuery = "DELETE FROM " + dbconfig.collaborators_table+
+									" WHERE " + dbconfig.collaborators_table + ".id=" + "'" + collab_id + "'";
+								handleDisconnect();
+								connection.query(removeCollabQuery, function(err, rows){
+					                    res.redirect('/profile');
+				                });
+							}
+						}
+						else{
+							res.redirect('/profile');
+						}
+                });
+			}
+		});
+
 		
 	});
 	// =============================================================================
@@ -502,7 +530,7 @@ module.exports = function(app, passport) {
 								res.redirect('/manageprojects/github'+'?project_name='+proj_name);
 							});
 						}//if there is only 1 owner, I delete the project
-						if(rows.length>0){
+						else{
 							res.redirect('/deleteprojects/github'+'?project_name='+proj_name);
 						}
 	                    
@@ -522,29 +550,110 @@ module.exports = function(app, passport) {
 		console.log(user);
 		//var proj_name = req.param('project_name');
 		var createProjectQuery = "INSERT INTO " + dbconfig.projects_table +
-				"(project_name)" +
+				" (project_name)" +
 				" VALUES ("+ "'" + proj_name+"'"+")";
 		console.log(createProjectQuery);
 		handleDisconnect();
 		connection.query(createProjectQuery, function(err, rows){
-			var getProjectId = "SELECT project_id FROM " + dbconfig.projects_table + " WHERE project_name="+ "'"
-						+ proj_name +"'";
-			handleDisconnect();
-			console.log(getProjectId);
-			connection.query(getProjectId, function(err, rows){
-				if(rows.length>0){
-					var createOwnerQuery = "INSERT INTO " +dbconfig.owners_table+ "(user_id,project_id)" +
-						" VALUES (" + "'"+ user.id + "'"+ ",'"+rows[0].project_id+"')";
-					handleDisconnect();
-					console.log(createOwnerQuery);
-					connection.query(createOwnerQuery, function(err, rows){
-						res.redirect('/manageprojects/github'+'?project_name='+proj_name);
-					});
-				}
-			});
+			if(rows){
+				var getProjectId = "SELECT project_id FROM " + dbconfig.projects_table + " WHERE project_name="+ "'"
+							+ proj_name +"'";
+				handleDisconnect();
+				console.log(getProjectId);
+				connection.query(getProjectId, function(err, rows){
+					if(rows.length>0){
+						var createOwnerQuery = "INSERT INTO " +dbconfig.owners_table+ "(user_id,project_id)" +
+							" VALUES (" + "'"+ user.id + "'"+ ",'"+rows[0].project_id+"')";
+						handleDisconnect();
+						console.log(createOwnerQuery);
+						connection.query(createOwnerQuery, function(err, rows){
+							res.redirect('/manageprojects/github'+'?project_name='+proj_name);
+						});
+					}
+					else{
+						res.redirect('/profile');
+					}
+				});
+			}
+			else{
+				res.redirect('/profile');
+			}
         }); 
 	});
-
+	// =============================================================================
+	// Add an Owner in a Project I own ========================================
+	// =============================================================================
+	app.post('/addOwner/github', isLoggedIn, function(req, res) {
+		var github_name = req.body.name;
+		var proj_name = req.body.project_name;
+		console.log('projectname to add owner '+proj_name);
+		var user = req.user;
+		var ownerflag;//flag to check if I am owner
+		checkIfOwner(user,proj_name,function(ownerflag){
+			if(ownerflag==true){
+				var checkIfUserExistsQuery = "SELECT id FROM " + dbconfig.users_table + " WHERE " +
+					dbconfig.users_table +".github_name=" + "'" + github_name +"'";
+				handleDisconnect();
+				connection.query(checkIfUserExistsQuery, function(err,rows){
+					if(rows.length>0){
+						var user_id = rows[0].id
+						var getProjectId = "SELECT project_id FROM " + dbconfig.projects_table + " WHERE project_name="+ "'"
+										+ proj_name +"'";
+						handleDisconnect();
+						connection.query(getProjectId, function(err, rows){
+							var createOwnerQuery = "INSERT INTO " +dbconfig.owners_table+ "(user_id,project_id)" +
+								" VALUES (" + "'"+ user_id + "'"+ ",'"+rows[0].project_id+"')";
+							handleDisconnect();
+							console.log(createOwnerQuery);
+							connection.query(createOwnerQuery, function(err, rows){
+								res.redirect('/manageprojects/github'+'?project_name='+proj_name);
+							});
+						});
+					}
+					else{
+						res.redirect('/manageprojects/github'+'?project_name='+proj_name);
+					}
+				});
+			}
+		});
+	});
+	// =============================================================================
+	// Add an Collaborator in a Project I own ======================================
+	// =============================================================================
+	app.post('/addCollaborator/github', isLoggedIn, function(req, res) {
+		var github_name = req.body.name;
+		var proj_name = req.body.project_name;
+		console.log('projectname to add owner '+proj_name);
+		var user = req.user;
+		var ownerflag;//flag to check if I am owner
+		checkIfOwner(user,proj_name,function(ownerflag){
+			if(ownerflag==true){
+				var checkIfUserExistsQuery = "SELECT id FROM " + dbconfig.users_table + " WHERE " +
+					dbconfig.users_table +".github_name=" + "'" + github_name +"'";
+				handleDisconnect();
+				connection.query(checkIfUserExistsQuery, function(err,rows){
+					if(rows.length>0){
+						var user_id = rows[0].id
+						var getProjectId = "SELECT project_id FROM " + dbconfig.projects_table + " WHERE project_name="+ "'"
+										+ proj_name +"'";
+						handleDisconnect();
+						connection.query(getProjectId, function(err, rows){
+							var createCollabQuery = "INSERT INTO " +dbconfig.collaborators_table+ "(user_id,project_id)" +
+								" VALUES (" + "'"+ user_id + "'"+ ",'"+rows[0].project_id+"')";
+							handleDisconnect();
+							console.log(createCollabQuery);
+							connection.query(createCollabQuery, function(err, rows){
+								res.redirect('/manageprojects/github'+'?project_name='+proj_name);
+							});
+						});
+					}
+					else{
+						res.redirect('/manageprojects/github'+'?project_name='+proj_name);
+					}
+				});
+			}
+		});
+	});
 };
 
 
