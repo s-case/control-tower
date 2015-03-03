209b2389d1ff4fd00560f278c61887cb17d6f2a6
Themis Mavridis <themismavridis@gmail.com>
2015-03-03 15:56:11 +0200

    initial attempt for display project names I own

--- app/functionallitiesAPI.js
+++ app/functionallitiesAPI.js
@@ -26,11 +26,14 @@ function handleDisconnect() {
 }
 // ROUTES FOR OUR API
 // =============================================================================
-	//route to delete a User
+	// =============================================================================
+	// Delete account API =============================================================
+	// =============================================================================
 	app.get('/api/deleteUser',function(req,res){
 		var scase_token=req.param('scase_token');
 		if(scase_token){
 			handleDisconnect();
+			//we select the user with the scase_token provided 
 			var selectUsersQuery = "SELECT * FROM " + dbconfig.users_table + " WHERE scase_token = '" + scase_token + "'";
 				connection.query(selectUsersQuery, function(err, rows){
 				   	if (err)
@@ -76,7 +79,9 @@ function handleDisconnect() {
 			});
 		}
 	});
-	//route to refresh the S-CASE token
+	// =============================================================================
+	// Refresh S-CASE token ACCOUNTS API===============================================
+	// =============================================================================
 	//we create a new scase token
 	var crypto = require('crypto');
 	var base64url = require('base64url');
@@ -88,6 +93,7 @@ function handleDisconnect() {
 		var scase_token=req.param('scase_token');
 		if(scase_token){
 			handleDisconnect();
+			//we select the user with the scase_token provided 
 			var selectUsersQuery = "SELECT * FROM " + dbconfig.users_table + " WHERE scase_token = '" + scase_token + "'";
 				connection.query(selectUsersQuery, function(err, rows){
 				   	if (err)
@@ -119,4 +125,50 @@ function handleDisconnect() {
 			});
 		}
 	});
+	// =============================================================================
+	// Display Projects I own API===============================================
+	// =============================================================================
+	//we create a new scase token
+	var crypto = require('crypto');
+	var base64url = require('base64url');
+	//function to crete an S-CASE token
+	function scasetokenCreate(size){
+	    return base64url(crypto.randomBytes(size));
+	}
+	app.get('/api/displayProjectsOwn',function(req,res){
+		var scase_token=req.param('scase_token');
+		if(scase_token){
+			handleDisconnect();
+			//we select the user with the scase_token provided 
+			var selectUsersQuery = "SELECT * FROM " + dbconfig.users_table + " WHERE scase_token = '" + scase_token + "'";
+				connection.query(selectUsersQuery, function(err, rows){
+				   	if (err)
+                        return done(err);
+                    if (rows.length > 0) {
+                    	var user = rows[0];
+                    	handleDisconnect();
+						//we select the user from the user's table (just to check if the user exists)
+						connection.query("SELECT * FROM " + dbconfig.users_table + " WHERE id = '" + user.id + "'", function(err, rows){
+				            if (rows.length > 0) {
+				                user = rows[0];
+				                //query for the projects I own
+								var projectsOwnedQuery = "SELECT `project_name` FROM " + dbconfig.projects_table +" JOIN " + dbconfig.owners_table + " ON "+ dbconfig.projects_table+
+									".`project_id` = "+ dbconfig.owners_table + ".`project_id` "+" WHERE "+ dbconfig.owners_table+".`user_id` = '" + user.id + "'";
+								connection.query(projectsOwnedQuery, function(err, rows){
+						            if (rows.length > 0) {
+						            	res.setHeader('Content-Type', 'application/json');
+											var obj = '{'
+													+ '"projectnames" : "'+rows+'"}';
+											var Jobj=JSON.parse(obj);
+											res.send(Jobj);
+									}
+					            });
+					        }
+					    });
+		            }
+			        
+			    });
+            }
+			
+		});
 };
