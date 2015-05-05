94da4823202a4239b50c90a0dbd8105451656180
Themis Mavridis <themismavridis@gmail.com>
2015-05-05 14:35:46 +0300

    disallow spaces in projects names

--- scripts/create_projects_table.sql
+++ scripts/create_projects_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `projects` (
   `project_id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(200) NOT NULL DEFAULT '',
+  `privacy_level` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`project_id`,`project_name`),
   UNIQUE KEY `project_name_UNIQUE` (`project_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
