eabf81e4880dd364232ef2bb9a9212041ece5373
Themis Mavridis <themismavridis@gmail.com>
2015-05-10 22:01:18 +0300

    fixed angular loading

--- app/routes/routeQAResults.js
+++ app/routes/routeQAResults.js
@@ -7,7 +7,7 @@ module.exports = function(app, passport) {
 	// Got to QA ======================================
 	// =============================================================================
 	app.get('/QA', isLoggedIn, function(req, res) {
-		res.render('/views/QA.html')
+		res.render('QA.html')
 	});
 };
 // route middleware to ensure user is logged i
